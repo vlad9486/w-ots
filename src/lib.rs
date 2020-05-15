61e3b61c45aeb7e62e7e95532418236772aabf79
Vladislav Melnik <vladislav.melnik@protonmail.com>
2020-05-15 11:27:43 +0300

    refactor

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,8 @@
 #![allow(non_shorthand_field_patterns)]
 
 mod state;
+mod signature;
 mod xmss;
 
 pub use self::state::WOtsPlus;
-pub use self::xmss::{SecretKey, PublicKey, Signature};
+pub use self::signature::{SecretKey, PublicKey, Signature};
